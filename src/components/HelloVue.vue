354bcaf7b8278576dfcf22d5b2ef8517dd074fe5
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-06-05 17:47:14 +0700

    feat: add HelloVue component, entry script, and multi-page build configuration for hello.html

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+console.info("Hello from HelloVue.vue")
+</script>
+
+<template>
+    <h1 class="hello"'>Hello Vue</h1>
+</template>
+
+<style scoped>
+.hello {
+    color: #42b983;
+}
+</style>
