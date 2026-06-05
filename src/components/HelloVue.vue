ce5e951a8f4257ba8ed0e7ffb1f4e22d5732dd39
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-06-05 20:34:14 +0700

    JS Expression di Template

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -9,12 +9,18 @@ const data = {
     id: "hello",
     class: "hello"
 }
+
+function helloFunc(){
+    return "Hello World";
+}
 </script>
 
 <template>
     <!-- Directive v-bind digunakan untuk mengisi id atau class secara dinamis -->
     <h1 v-bind:id="idHello" v-bind:class="classHello">{{ hello }}</h1>
-    <h1 class="hello">{{ hello }}</h1>
+    <h1 class="hello">{{ hello.toUpperCase }}</h1>
+    <h1 class="hello">{{ 10 * 10 }}</h1>
+    <h2>{{ helloFunc() }}</h2>
     <div v-bind:class="data.class" v-html="hello"></div>
     <div v-bind="data" v-html="hello"></div>
     <!-- Directive v-html digunakan untuk menampilkan data dalam bentuk html -->
