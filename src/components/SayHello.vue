28b0ace1943e6988bda5e41b2632d6fc6b078ed5
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-06-07 04:19:07 +0700

    Event Handling

--- src/components/SayHello.vue
+++ src/components/SayHello.vue
@@ -1,24 +1,37 @@
 <template>
     <div>
-        <button v-on:click="increment">Increment {{ counter }}</button>
-        <div class="row">
-            <label for="firstName">First Name</label>
-            <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                placeholder="First Name"
-            />
-        </div>
-        <br />
-        <div class="row">
-            <label for="lastName">Last Name</label>
-            <input type="text" id="lastName" name="lastName" />
-        </div>
-        <br />
-        <button v-on:click="sayHello">Say Hello</button>
+        <form>
+            <button v-on:click="increment">Increment {{ counter }}</button>
+            <div class="row">
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" placeholder="First Name" />
+            </div>
+            <br />
+            <div class="row">
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" />
+            </div>
+            <br />
+            <!-- Event modifer -> modifier adalah untuk memodifikasi event handler, contohnya prevent untuk mencegah default behavior dari sebuah event (misalnya submit form). Modifier ini bisa digabungkan, contohnya .prevent.stop untuk mencegah default behavior dan menghentikan event dari menyebar ke elemen lain. -->
+            <button v-on:click.prevent="sayHello">Say Hello</button>
+        </form>
     </div>
-    <h1>Hello {{ getFullName }}</h1>
+    <h2>Hello {{ getFullName }}</h2>
+    <ul>
+        <li>
+            <h1 :class="['red']">Directive event handling</h1>
+        </li>
+    </ul>
+    <div class="row">
+        <label for="firstName">First Name</label>
+        <input type="text" id="firstNameEvent" name="firstName" placeholder="First Name" v-on:input="changeFirstName" />
+    </div>
+    <div class="row">
+        <label for="lastName">Last Name</label>
+        <input type="text" id="lastNameEvent" name="lastName" v-on:input="changeLastName" />
+    </div>
+    <br />
+    <button v-on:click="counter++">Increment inline {{ counter }}</button>
 </template>
 
 <script setup>
@@ -47,6 +60,19 @@ const getFullName = computed((oldname) => {
     console.log(`Old name: ${oldname}`);
     return `${person.firstName} ${person.lastName}`;
 });
+
+// Event handler -> function yang dipanggil ketika terjadi event, seperti click, input, dll. Event handler akan menerima parameter event yang berisi informasi tentang event yang terjadi, seperti target, type, dll.
+function changeFirstName(event) {
+    // cara lama:
+    // person.firstName = document.getElementById("firstNameEvent").value;
+    person.firstName = event.target.value;
+}
+
+function changeLastName(event) {
+    // cara lama:
+    // person.lastName = document.getElementById("lastNameEvent").value;
+    person.lastName = event.target.value;
+}
 </script>
 
 <style scoped>
@@ -54,5 +80,9 @@ const getFullName = computed((oldname) => {
     display: flex;
     align-items: center;
     gap: 10px;
+}
+
+.red {
+    color: red;
 }
 </style>
